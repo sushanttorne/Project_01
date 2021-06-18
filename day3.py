e75393b8035443575b30a6cb7cb13418606ee224
sushanttorne <85859083+sushanttorne@users.noreply.github.com>
2021-06-18 10:20:54 +0530

    Python_1

---
+++ day3.py
@@ -0,0 +1,11 @@
+list1 = ["apple","mango"]
+
+list2 = ["apple","mango"]
+
+x = list1
+
+print(list1 is x)
+
+print(list1 == list2)
+
+print(list1 is list2)
---
+++ python.py
@@ -0,0 +1,7 @@
+num1 = 10
+
+num2 = 30 
+
+ans= num1 + num2
+ 
+print('Your awnser is -' ,+ans) 
